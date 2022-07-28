0f40c6a10474bf983fa2ed2729e0b56af4b25b5c
tomashina <tomislav@agmedia.hr>
2022-07-28 09:52:15 +0200

    kekspay

---
+++ upload/catalog/controller/extension/payment/kekspay.php
@@ -0,0 +1,166 @@
+<?php
+require_once DIR_APPLICATION . 'controller/extension/payment/kekspay/autoload.php';
+
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+
+class ControllerExtensionPaymentKeksPay extends Controller
+{
+    public function index()
+    {
+        $this->load->model('checkout/order');
+        
+        $this->load->language('extension/payment/kekspay');
+        
+        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+        
+        if ( ! $this->config->get('payment_kekspay_test')) {
+            $data['action'] = 'https://kekspayuat.erstebank.hr/eretailer/';
+        } else {
+            //$data['action'] = 'https://dttlinuxdev.erste.hr/tps';
+            $data['action'] = 'https://kekspayuat.erstebank.hr/eretailer/';
+        }
+        
+        if ($this->request->server['HTTPS']) {
+            $data['logo'] = HTTPS_SERVER . 'image/keks-logo.svg';
+        } else {
+            $data['logo'] = HTTP_SERVER . 'image/keks-logo.svg';
+        }
+        
+        if ($this->request->server['HTTPS']) {
+            $success_url = HTTPS_SERVER . 'index.php?route=extension/payment/kekspay/success';
+            $fail_url    = HTTPS_SERVER . 'index.php?route=extension/payment/kekspay/fail';
+        } else {
+            $success_url = HTTP_SERVER . 'index.php?route=extension/payment/kekspay/success';
+            $fail_url    = HTTP_SERVER . 'index.php?route=extension/payment/kekspay/fail';
+        }
+        
+        $store_name = $this->config->get('payment_kekspay_shop_title') != '' ? $this->config->get('payment_kekspay_shop_title') : 'Trgovina';
+        
+        $data['qr_code']     = 1;
+        $data['cid']         = $this->config->get('payment_kekspay_cid');
+        $data['tid']         = $this->config->get('payment_kekspay_tid');
+        $data['bill_id']     = $this->config->get('payment_kekspay_cid') . time() . $order_info['order_id'];
+        $data['amount']      = number_format($order_info['total'], 2, '.', '');
+        $data['store']       = rawurlencode($store_name);
+        $data['success_url'] = rawurlencode($success_url);
+        $data['fail_url']    = rawurlencode($fail_url);
+        
+        $data['button_confirm'] = $this->language->get('kekspay_btn_confirm');
+        $data['order_id']       = $order_info['order_id'];
+        
+        $options = new QROptions([
+            'version'          => 6,
+            'quietzoneSize'    => 4,
+            'eccLevel'         => QRCode::ECC_L,
+            'imageTransparent' => false,
+        ]);
+        
+        $qrdata = [
+            "qr_type" => 1,
+            "cid"     => $this->config->get('payment_kekspay_cid'),
+            "tid"     => $this->config->get('payment_kekspay_tid'),
+            "bill_id" => $this->config->get('payment_kekspay_cid') . time() . $order_info['order_id'],
+            "amount"  => number_format($order_info['total'], 2, '.', ''),
+            "store"   => rawurlencode($store_name),
+        ];
+        
+        $qrcode = new QRCode($options);
+        
+        $data['qrcode'] = $qrcode->render(json_encode($qrdata));
+        
+        return $this->load->view('extension/payment/kekspay', $data);
+    }
+    
+    
+    public function callback()
+    {
+        $this->load->model('checkout/order');
+        
+        $json_response = json_decode(file_get_contents('php://input'), true);
+        
+        \Agmedia\Helpers\Log::write('KeksPay Advice on callback() :::::::::', 'callback');
+        \Agmedia\Helpers\Log::write($json_response, 'callback');
+        
+        $order_id = substr($json_response['bill_id'], 16);
+        
+        if ( ! $json_response['status']) {
+            $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('payment_kekspay_order_status_id'), '', true);
+            
+            $this->response->addHeader('Content-Type: application/json');
+            return $this->response->setOutput(json_encode([
+                'status'  => 0,
+                'message' => 'Accepted'
+            ]));
+            
+        } else {
+            $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('config_order_status_id'), '', true);
+            
+            $this->response->addHeader('Content-Type: application/json');
+            return $this->response->setOutput(json_encode([
+                'status'  => 1,
+                'message' => 'Failed'
+            ]));
+        }
+        
+    }
+    
+    
+    public function check()
+    {
+        $json           = [];
+        $json['status'] = 0;
+    
+        $this->load->model('checkout/order');
+        $order_info = $this->model_checkout_order->getOrder($this->request->post['order_id']);
+        
+        if ($order_info['order_status_id']) {
+            $json['redirect'] = $this->url->link('checkout/checkout');
+            $json['status']   = 1;
+            
+            if ($order_info['order_status_id'] == $this->config->get('payment_kekspay_order_status_id')) {
+                $json['redirect'] = $this->url->link('checkout/success');
+            }
+        }
+        
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+    
+    
+    public function success()
+    {
+        $json_response = json_decode(file_get_contents('php://input'), true);
+        
+        \Agmedia\Helpers\Log::write($_POST, 'success');
+        \Agmedia\Helpers\Log::write($json_response, 'success');
+    }
+    
+    
+    public function fail()
+    {
+        $json_response = json_decode(file_get_contents('php://input'), true);
+        
+        \Agmedia\Helpers\Log::write($_POST, 'fail');
+        \Agmedia\Helpers\Log::write($json_response, 'fail');
+    }
+
+    public function verify_kekspay_token() {
+        $token_src = 'REQUEST';
+        $token     = isset( $_REQUEST['token'] ) ? filter_var( stripslashes( $_REQUEST['token'] ), FILTER_SANITIZE_STRING ) : false;
+        if ( ! $token ) { // Legacy check.
+            $token_src = 'REQUEST';
+            $token     = isset( $_SERVER['HTTP_AUTHORIZATION'] ) ? filter_var( $_SERVER['HTTP_AUTHORIZATION'], FILTER_SANITIZE_STRING ) : false;
+        }
+        if ( ! $token ) {
+            Kekspay_Logger::log( 'Failed to recieve authentication token.', 'error' );
+            $this->respond_error( 'Authentication token missing, failed to verify.' );
+        } else {
+            Kekspay_Logger::log( 'Token src: ' . $token_src, 'info' );
+        }
+
+        return hash_equals( Kekspay_Data::get_settings( 'auth-token' ), str_replace( 'Token ', '', $token ) );
+    }
+}
+
+?>
---
+++ upload/catalog/language/hr-hr/extension/payment/kekspay.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']     = 'Thank you for shopping with %s .... ';
+
+// Text
+$_['text_title']        = '<span>KEKS Pay</span></br><span style="font-size: 13px;">Najbrže i bez naknada putem KEKS Pay aplikacije!</span> <img src="image/keks-logo.svg" class="payment-logo">';
+$_['text_response']     = 'Response from WSPay:';
+$_['text_success']      = '... your payment was successfully received.';
+$_['text_success_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b> whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
+$_['text_failure']      = '... Your payment has been cancelled!';
+$_['text_failure_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b><br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
+$_['text_pw_mismatch']  = 'CallbackPW does not match. Order requires investigation.';
+
+$_['kekspay_btn_confirm']  = 'Otvori KEKS Pay';
+
+?>
