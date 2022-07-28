972e41654067e4129f939fcce8211b8356544a24
tomashina <tomislav@agmedia.hr>
2022-07-28 10:39:35 +0200

    kekspay

--- upload/catalog/controller/extension/payment/kekspay.php
+++ upload/catalog/controller/extension/payment/kekspay.php
@@ -81,7 +81,7 @@ class ControllerExtensionPaymentKeksPay extends Controller
         
         \Agmedia\Helpers\Log::write('KeksPay Advice on callback() :::::::::', 'callback');
         \Agmedia\Helpers\Log::write($json_response, 'callback');
-        \Agmedia\Helpers\Log::write($_POST, 'callback');
+        \Agmedia\Helpers\Log::write($_REQUEST['token'], 'callback');
         \Agmedia\Helpers\Log::write($_GET, 'callback');
 
         
