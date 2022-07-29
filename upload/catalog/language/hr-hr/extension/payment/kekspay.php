07b9a660a381b83a22f7bad5b9fd6b4c2752e9d7
tomashina <tomislav@agmedia.hr>
2022-07-29 09:58:53 +0200

    kekspay

--- upload/catalog/language/hr-hr/extension/payment/kekspay.php
+++ upload/catalog/language/hr-hr/extension/payment/kekspay.php
@@ -3,7 +3,7 @@
 $_['heading_title']     = 'Thank you for shopping with %s .... ';
 
 // Text
-$_['text_title']        = '<span>KEKS Pay</span></br><span>Najbrže i bez naknada putem KEKS Pay aplikacije!</span> <img src="image/keks-logo.svg" class="payment-logo">';
+$_['text_title']        = '<span>KEKS Pay</span></br><span> Najbrže i bez naknada putem KEKS Pay aplikacije!</span> <img src="image/keks-logo.svg" class="payment-logo">';
 $_['text_response']     = 'Response from WSPay:';
 $_['text_success']      = '... your payment was successfully received.';
 $_['text_success_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b> whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
