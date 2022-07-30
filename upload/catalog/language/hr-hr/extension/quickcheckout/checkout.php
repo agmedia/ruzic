807e796044dfa081137cecbe7c9a91bcee9de84c
tomashina <tomislav@agmedia.hr>
2022-07-30 11:20:14 +0200

    min order

--- upload/catalog/language/hr-hr/extension/quickcheckout/checkout.php
+++ upload/catalog/language/hr-hr/extension/quickcheckout/checkout.php
@@ -57,7 +57,7 @@ $_['error_delivery']				 = 'Warning: Delivery date is required!';
 $_['error_maximum']      		     = 'Warning: The maximum number of points that can be applied is %s!';
 $_['error_reward']        		     = 'Warning: Please enter the amount of reward points to use!';	
 $_['error_points']           		 = 'Warning: You don\'t have %s reward points!';
-$_['error_minimum_order']			 = 'Upozorenje: MInimalna narudžba je %s.';
+$_['error_minimum_order']			 = 'Upozorenje: Minimalna narudžba je u iznosu od %s.';
 $_['error_comment']					 = 'Warning: Order comment is required.';
 
 // Text
