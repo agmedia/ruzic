8bc59035855ec9ae126c86eac38eb5a15c219fb6
tomashina <tomislav@agmedia.hr>
2022-08-01 15:00:00 +0200

    pakiranje list

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -204,6 +204,7 @@ class ControllerProductCategory extends Controller {
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
+					'pakiranje' => $result['location'],
 					'tax'         => $tax,
 					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 					'rating'      => $result['rating'],
