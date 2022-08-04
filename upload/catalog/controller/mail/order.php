7f1563049cc03cabb2b33d2a6918b439118fe46b
tomashina <tomislav@agmedia.hr>
2022-08-04 14:00:35 +0200

    total

--- upload/catalog/controller/mail/order.php
+++ upload/catalog/controller/mail/order.php
@@ -259,14 +259,10 @@ class ControllerMailOrder extends Controller {
 
 		foreach ($order_totals as $order_total) {
 
-            if($order_info['currency_code']!='EUR'){
-                $tekst = '<small>'.$this->currency->format($order_total['value'], 'EUR'). '</small> ';
-            }else{
-                $tekst = '';
-            }
+
 			$data['totals'][] = array(
 				'title' => $order_total['title'],
-				'text'  => $tekst.$this->currency->format($order_total['value'], $order_info['currency_code'], $order_info['currency_value']),
+				'text'  => $this->currency->format($order_total['value'], $order_info['currency_code'], $order_info['currency_value']),
 			);
 		}
 	
