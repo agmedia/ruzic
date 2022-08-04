34845a941113645fb547f1bda5e7bc29aa7cc9c5
tomashina <tomislav@agmedia.hr>
2022-08-04 12:24:03 +0200

    checkout

--- upload/catalog/language/hr-hr/extension/shipping/flat.php
+++ upload/catalog/language/hr-hr/extension/shipping/flat.php
@@ -1,5 +1,3 @@
 <?php
-// Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
-// Text
 $_['text_title']       = 'Dostavnom službom GLS u roku 1 do 3 radna dana';
 $_['text_description'] = 'Dostavnom službom GLS u roku 1 do 3 radna dana';
