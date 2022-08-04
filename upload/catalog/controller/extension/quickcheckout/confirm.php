e18733b602b9d7b2903c9f1cd9ae814b1ccdd056
tomashina <tomislav@agmedia.hr>
2022-08-04 15:15:16 +0200

    total

--- upload/catalog/controller/extension/quickcheckout/confirm.php
+++ upload/catalog/controller/extension/quickcheckout/confirm.php
@@ -446,7 +446,18 @@ class ControllerExtensionQuickCheckoutConfirm extends Controller {
 			$data['totals'] = array();
 
 			foreach ($order_data['totals'] as $total) {
-				$text = $this->currency->format($total['value'], $this->session->data['currency']);
+				//$text = $this->currency->format($total['value'], $this->session->data['currency']);
+
+
+                if($this->session->data['currency']=='HRK'){
+
+                    $text = '<small>'.$this->currency->format($total['value'], 'EUR'). '</small> '. $this->currency->format($total['value'], $this->session->data['currency']);
+
+                }
+
+                else{
+                    $text = $this->currency->format($total['value'], $this->session->data['currency']);
+                }
 				
 				$data['totals'][] = array(
 					'title' => $total['title'],
