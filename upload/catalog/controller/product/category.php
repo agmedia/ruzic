f02fc4f6ff46da61b28e061fdd84062b303bf311
tomashina <tomislav@agmedia.hr>
2022-08-18 08:53:06 +0200

    fixes category bottom module

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -441,7 +441,14 @@ class ControllerProductCategory extends Controller {
 			$data['column_left'] = $this->load->controller('common/column_left');
 			$data['column_right'] = $this->load->controller('common/column_right');
 			$data['content_top'] = $this->load->controller('common/content_top');
-			$data['content_bottom'] = $this->load->controller('common/content_bottom');
+
+			if($this->cart->hasProducts()){
+                $data['content_bottom'] = $this->load->controller('common/content_bottom');
+            }
+			else{
+                $data['content_bottom'] ='';
+            }
+
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
 
