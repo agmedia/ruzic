af8b08f920f8ba3c14f7c9985e56e17fc09102e6
tomashina <tomislav@agmedia.hr>
2022-10-20 15:46:20 +0200

    terms

--- upload/catalog/controller/extension/quickcheckout/terms.php
+++ upload/catalog/controller/extension/quickcheckout/terms.php
@@ -3,14 +3,14 @@ class ControllerExtensionQuickCheckoutTerms extends Controller {
   	public function index() {
 		$data = $this->load->language('checkout/checkout');
 		$data = array_merge($data, $this->load->language('extension/quickcheckout/checkout'));
-        $this->config->get('config_checkout_id') = 5;
+
 		if ($this->config->get('config_checkout_id')) {
 			$this->load->model('catalog/information');
 			
-			$information_info = $this->model_catalog_information->getInformation($this->config->get('config_checkout_id'));
+			$information_info = $this->model_catalog_information->getInformation(5);
 			
 			if ($information_info) {
-				$data['text_agree'] = sprintf($this->language->get('text_agree'), $this->url->link('information/information/agree', 'information_id=' . $this->config->get('config_checkout_id'), true), $information_info['title'], $information_info['title']);
+				$data['text_agree'] = sprintf($this->language->get('text_agree'), $this->url->link('information/information/agree', 'information_id=5', $information_info['title'], $information_info['title']);
 			} else {
 				$data['text_agree'] = '';
 			}
